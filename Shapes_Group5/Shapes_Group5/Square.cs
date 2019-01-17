ea3a3c937c0341f2d191a74d62d3e363c28dc2e1
kaveshy <kkarunakaram@retrorabbit.co.za>
2019-01-17 11:37:08 +0200

    new fixes

--- Shapes_Group5/Shapes_Group5/Square.cs
+++ Shapes_Group5/Shapes_Group5/Square.cs
@@ -9,7 +9,19 @@ namespace Shapes_Group5
         int area = 0;
         public Square()
         {
+            int length = 0;
+            int width = 0;
+            Console.WriteLine("Provide the length of the square");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Provide the width of the square");
+            width = Convert.ToInt32(Console.ReadLine());
+     
+            int area = 0;
 
+            area = calcSquareArea(length, width);
+
+            Console.WriteLine("the are is: " + area);
+            Console.ReadLine();
         }
 
         public int calcSquareArea(int length, int width)
@@ -25,4 +37,4 @@ namespace Shapes_Group5
         }
     }
 }
-}
+
