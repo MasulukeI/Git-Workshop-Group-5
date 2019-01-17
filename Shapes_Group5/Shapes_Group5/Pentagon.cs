7481080f311447cfce3015d58b4a4cf74261a20d
MpiloMshengu <mmshengu@retrorebbit.co.za>
2019-01-17 10:51:30 +0200

    Pentagon

---
+++ Shapes_Group5/Shapes_Group5/Pentagon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes_Group5
+{
+    class Pentagon
+    {
+        private float a;
+        private float r;
+        public  float area;
+        public float Perimeter;
+      
+        Pentagon(float a, float r)
+        {
+            a = this.a;
+            a = this.r;
+        }
+
+        public void CalArea(float a, float r) {
+            area = (5 * r * a) / 2;
+
+        }
+        public void CalPerimeter (){
+            Perimeter = 5 * a;
+        }
+
+
+
+    }
+}
