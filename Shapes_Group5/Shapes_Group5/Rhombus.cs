417e3a0d5d0020230088127131d9673e14942be9
GiftLanga <glanga@retrorabbit.co.za>
2019-01-17 10:47:36 +0200

    Created the rhombus class

---
+++ Shapes_Group5/Shapes_Group5/Rhombus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes_Group5
+{
+    class Rhombus
+    {
+        int _length;
+        int _altitude;
+        public Rhombus(int length, int altitude)
+        {
+            _length = length;
+            _altitude = altitude;
+        }
+
+        public int getRhombuArea()
+        {
+            return _length * _altitude;
+        }
+    }
+}
