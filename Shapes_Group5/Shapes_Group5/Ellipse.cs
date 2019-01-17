f40c0f5e36c932c8a5d56dacdac8732bf665370d
mthembbu <bmthembu@retrorabbit.co.za>
2019-01-17 11:06:11 +0200

    The class simply calculates the area of the Eclipse

---
+++ Shapes_Group5/Shapes_Group5/Ellipse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes_Group5
+{
+    class Ellipse
+    {
+        double a, b;
+
+        public Ellipse() {
+            this.getInputs();
+        }
+        public Ellipse(double a, double b) {
+            this.a = a;
+            this.b = b;
+            }
+        public void getArea() {
+            Console.WriteLine("The area of the Ellipse is "+ (Math.PI*a*b).ToString());
+        }
+
+        public void getInputs() {
+
+            Console.WriteLine("Please enter th ex-value: ");
+           this.a= Convert.ToDouble(Console.Read());
+            Console.WriteLine("Please enter th ex-value: ");
+            this.b = Convert.ToDouble(Console.Read());
+
+}
+    }
+}
