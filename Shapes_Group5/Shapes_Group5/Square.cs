099be0a654ff2609613327fbee526d507fe6be9e
kaveshy <kkarunakaram@retrorabbit.co.za>
2019-01-17 10:45:57 +0200

    this is the square

---
+++ Shapes_Group5/Shapes_Group5/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes_Group5
+{
+    class Square
+    {
+        int area = 0;
+        public Square()
+        {
+
+        }
+
+        public int calcSquareArea(int length, int width)
+        {
+
+             area =length * width;
+            return area;
+        }
+
+        public int getSquareArea()
+        {
+            return area;
+        }
+    }
+}
+}
